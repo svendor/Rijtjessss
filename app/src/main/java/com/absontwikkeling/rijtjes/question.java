873320a359d5e6bebcbb404ab05015abb79fdad3
svendor <svendor@users.noreply.github.com>
2017-09-20 15:10:08 +0200

    More changes: nice

---
+++ app/src/main/java/com/absontwikkeling/rijtjes/question.java
@@ -0,0 +1,17 @@
+package com.absontwikkeling.rijtjes;
+
+/**
+ * Created by Sven de Ronde on 20-9-2017.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class question extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question);
+    }
+}
