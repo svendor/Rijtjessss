7524df4e6f3b12d7d9a04acfecdc0d9e6f8b6691
svendor <svendor@users.noreply.github.com>
2017-09-27 21:37:29 +0200

    Added: - question.java   - Method (checkAnswer) to check answers   - Proof of concept UI with above method
    - activity_question.xml
      - 2 checkboxes (whitespace + capitals)
      - 2 EditText's (inputString + checkAnswer)

--- app/src/main/java/com/absontwikkeling/rijtjes/MainActivity.java
+++ app/src/main/java/com/absontwikkeling/rijtjes/MainActivity.java
@@ -1,14 +1,30 @@
 package com.absontwikkeling.rijtjes;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private void setupQuestionButtonListener(){
+        Button questionButton = (Button) findViewById(R.id.questionLink);
+        questionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent intQuestion = new Intent(MainActivity.this, question.class);
+                startActivity(intQuestion);
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setupQuestionButtonListener();
     }
 }
 
