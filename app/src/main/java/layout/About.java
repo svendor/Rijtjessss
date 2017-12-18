c22ab39a220ac1bc17d496d5b1e39dc436f080fe
Bassie-c <6718344+bassie-c@users.noreply.github.com>
2017-12-18 21:25:35 +0100

    Version number displayed in the app, under the about(Over) tab

--- app/src/main/java/layout/About.java
+++ app/src/main/java/layout/About.java
@@ -2,12 +2,20 @@ package layout;
 
 
 import android.os.Bundle;
+import android.preference.EditTextPreference;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.absontwikkeling.rijtjes.BuildConfig;
 import com.absontwikkeling.rijtjes.R;
+
+import org.w3c.dom.Text;
+
+import java.text.CollationElementIterator;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +32,16 @@ public class About extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view = inflater.inflate(R.layout.fragment_about, container, false);
+
+        TextView versionInfo = (TextView) view.findViewById(R.id.versionInfo);
+
+        String stringCodeText = getResources().getString(R.string.versionCode);
+        String stringNameText = getResources().getString(R.string.versionName);
+
+        versionInfo.setText(stringNameText + BuildConfig.VERSION_NAME + "; " +  stringCodeText + BuildConfig.VERSION_CODE);
+
+        return view;
     }
 
 }
